2da94f1c9377bb21cf8860e842e2a56205376ce8
celine.vialard <celine.vialard@gmail.com>
2021-10-17 15:10:25 +0200

    insert Restaurant script

---
+++ Data/StructureDB/InsertRestaurants.sql
@@ -0,0 +1,16 @@
+﻿INSERT INTO Restaurant(locationid, restaurantname, restaurantdescription )
+VALUES(4011,'Chez Tao', N'Déclaration de viande
+Poulet: Brésil
+Canard: Hongrie
+Bœuf: Suisse
+Porc: Suisse
+Poisson: Vietnam
+Crevettes: Vietnam');
+INSERT INTO Restaurant(locationid, restaurantname, restaurantdescription )
+VALUES(4011,'Beefore', N'Artisan du hamburger depuis 2016, nous privilégions les produits frais et locaux avec lesquels nous vous préparons de délicieux burgers. Notre menu est composé des grands classiques ainsi que de burgers créés au gré des saisons')
+INSERT INTO Restaurant(locationid, restaurantname, restaurantdescription )
+VALUES(4011, 'Crêperie Le Rustique',N'TOUTES LES SAUCES QUI GARNISSENT NOS CRÊPES 
+SONT MIJOTÉES PAR LE PATRON ET NOS PÂTES À CRÊPES SONT FAITES MAISON 
+AVEC DES ŒUFS SUISSES ÉLEVÉS AU SOL ET TOUTES NOS VIANDES D''ORIGINE SUISSE.');
+INSERT INTO Restaurant(locationid, restaurantname, restaurantdescription )
+VALUES(4063 ,'Mâe Khong Chez Pom',N'Restaurant thaïlandais à Saint-Maurice')
