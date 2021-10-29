f50b57035f4b384d8792a3dfc61588f4e39e0cdf
celine.vialard <celine.vialard@gmail.com>
2021-10-29 12:52:55 +0200

    add project to test BLL

---
+++ TestBLL/Program.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.IO;
+
+namespace TestBLL
+{
+	class Program
+	{
+		public static IConfiguration Configuration { get; } = new ConfigurationBuilder()
+			.SetBasePath(Directory.GetCurrentDirectory())
+			.AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+				 .Build();
+
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+		}
+	}
+}
