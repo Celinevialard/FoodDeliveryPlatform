c68465f2e4c00c71a0c369b933c141deff353653
celine.vialard <celine.vialard@gmail.com>
2021-10-29 14:10:33 +0200

    get dishes by restaurants

--- Data/DB/DishesDB.cs
+++ Data/DB/DishesDB.cs
@@ -1,12 +1,73 @@
-﻿using System;
+﻿using DTO;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
 
 namespace DAL
 {
   public class DishesDB
   {
-    // Constructeur
+		//Constructeur
+		private IConfiguration Configuration { get; }
 
-    // pouvoir liste
+		public DishesDB(IConfiguration configuration)
+		{
+			Configuration = configuration;
+		}
 
-  }
+		// pouvoir liste
+		public List<Dish> GetDishesByRestaurantId(int restaurantId)
+		{
+			List<Dish> results = null;
+			string connectionString = Configuration.GetConnectionString("DefaultConnection");
+
+			try
+			{
+				using (SqlConnection cn = new SqlConnection(connectionString))
+				{
+					string query = "Select * from Dish WHERE restaurantId = @restaurantId";
+					SqlCommand cmd = new SqlCommand(query, cn);
+					cmd.Parameters.AddWithValue("@restaurantId", restaurantId);
+					cn.Open();
+
+					using (SqlDataReader dr = cmd.ExecuteReader())
+					{
+						while (dr.Read())
+						{
+							if (results == null)
+								results = new List<Dish>();
+
+							results.Add(ReadDish(dr));
+
+						}
+					}
+				}
+			}
+			catch (Exception e)
+			{
+				throw e;
+			}
+
+			return results;
+
+		}
+
+		private Dish ReadDish(SqlDataReader dr)
+		{
+			Dish dish = new Dish();
+
+			dish.DishId = (int)dr["dishId"];
+			dish.Name = (string)dr["dishName"];
+			dish.Description = (string)dr["description"];
+			dish.Allergies = (string)dr["allergies"];
+			dish.Price = (decimal)dr["price"];
+			dish.ImageLink = (string)dr["image"];
+
+			dish.RestaurantId = (int)dr["restaurantId"];
+		
+			return dish;
+		}
+
+	}
 }
