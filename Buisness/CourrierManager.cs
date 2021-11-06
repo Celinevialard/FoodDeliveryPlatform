4aac526db55a151b1f0007d7ca473e1d8e21463d
François Brouchoud <francois.brouchoud@hevs.ch>
2021-11-06 18:01:05 +0100

    comment : add

--- Buisness/CourrierManager.cs
+++ Buisness/CourrierManager.cs
@@ -18,13 +18,15 @@ namespace BLL
             CourrierDb = new CourriersDB(conf);
         }
         /// <summary>
-        /// Création d'un nouveau livreur
+        /// Création d'un nouveau livreur étant déjà dans la base de person
         /// </summary>
         /// <param name="courrier"></param>
         public void AddCourrier(Courrier courrier)
         {
             CourrierDb.AddCourrier(courrier);
         }
+
+
 
         /// <summary>
         /// Obtenir les zones de livraison d'un livreur
